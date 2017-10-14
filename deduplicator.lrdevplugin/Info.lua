9923db9fd549c4922ec2547ae570febf9b83960d
Igor Shishkin <me@teran.ru>
2017-10-15 02:02:25 +0300

    Set Info.lua to be better usable for other modules
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- deduplicator.lrdevplugin/Info.lua
+++ deduplicator.lrdevplugin/Info.lua
@@ -1,16 +1,21 @@
+plugin_name = LOC "$$$/PluginInfo/Name=Deduplicator"
+plugin_major = 1
+plugin_minor = 0
+plugin_revision = 0
+plugin_build = 100002
+plugin_version = plugin_major .. '.' .. plugin_minor .. '.' .. plugin_revision .. '.' .. plugin_build
+plugin_id = 'me.teran.lightroom.deduplicator'
+plugin_home_url = "https://github.com/teran/deduplicator"
+
 return {
-
         LrSdkVersion = 6.0,
-
-        LrToolkitIdentifier = 'me.teran.lightroom.deduplicator',
-        LrPluginName = LOC "$$$/PluginInfo/Name=Deduplicator",
-        LrPluginInfoUrl = "https://github.com/teran/deduplicator",
-
+        LrToolkitIdentifier = plugin_id,
+        LrPluginName = plugin_name,
+        LrPluginInfoUrl = plugin_home_url,
         LrLibraryMenuItems = {
           title = 'Find duplicates',
           file = 'FindDuplicates.lua',
           enabledWhen = 'photosAvailable',
         },
-
-        VERSION = { major=1, minor=0, revision=0, build=100001, },
+        VERSION = { major=plugin_major, minor=plugin_minor, revision=plugin_revision, build=plugin_build, },
 }
