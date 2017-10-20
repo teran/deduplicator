7e2b7f4f99d2e3a639da0709ba15998ca6b542b4
Igor Shishkin <me@teran.ru>
2017-10-20 23:23:12 +0300

    Add logs to GithubCheckUpdates
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- deduplicator.lrdevplugin/GithubCheckUpdates.lua
+++ deduplicator.lrdevplugin/GithubCheckUpdates.lua
@@ -1,5 +1,6 @@
 local LrDialogs = import 'LrDialogs'
 local LrHttp = import 'LrHttp'
+local LrLogger = import 'LrLogger'
 local LrTasks = import 'LrTasks'
 
 local json = require 'JSON'
@@ -7,9 +8,16 @@ json.strictTypes = true
 
 require 'Info'
 
+local logger = LrLogger('Deduplicator')
+logger:enable('logfile')
+
+logger:trace('GithubCheckUpdates.lua invoked')
+
 function checkForUpdates()
+  logger:debugf('Preparing to request %s', latestReleaseJsonUrl)
   local responseBody, headers = LrHttp.get(latestReleaseJsonUrl)
   r = json:decode(responseBody)
+  logger:infof('Received latest available version as %s', r['tag_name'])
   if r['tag_name'] ~= plugin_version then
     local referToNewRelease = LrDialogs.confirm(
       'Update is available!',
@@ -19,9 +27,13 @@ function checkForUpdates()
     )
 
     if referToNewRelease == 'ok' then
+      logger:tracef('Opening %s in system browser', r['html_url'])
       LrHttp.openUrlInBrowser(r['html_url'])
+    else
+      logger:tracef("User refused to update :'(")
     end
   else
+    logger:infof('Deduplicator is up-to-date')
     LrDialogs.message('Deduplicator is up-to-date!', 'Keep it going!')
   end
 end
