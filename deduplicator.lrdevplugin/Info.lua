a48e5b6fc2ab26b8a849c15b1dc95b0a9286f7f7
Igor Shishkin <me@teran.ru>
2017-10-24 16:23:21 +0300

    plugin revision bump
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- deduplicator.lrdevplugin/Info.lua
+++ deduplicator.lrdevplugin/Info.lua
@@ -10,7 +10,7 @@ GIT Repository with the code:
 plugin_name = LOC "$$$/PluginInfo/Name=Deduplicator"
 plugin_major = 1
 plugin_minor = 0
-plugin_revision = 0
+plugin_revision = 1
 plugin_build = 100009
 plugin_version = plugin_major .. '.' .. plugin_minor .. '.' .. plugin_revision .. '.' .. plugin_build
 plugin_id = 'me.teran.lightroom.deduplicator'
