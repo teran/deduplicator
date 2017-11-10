b23807922677e65da3a33de9a533c51d36ef663e
Igor Shishkin <me@teran.ru>
2017-11-11 01:58:59 +0300

    Version bump + string.formt for version
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- deduplicator.lrdevplugin/Info.lua
+++ deduplicator.lrdevplugin/Info.lua
@@ -10,9 +10,9 @@ GIT Repository with the code:
 plugin_name = LOC "$$$/PluginInfo/Name=Deduplicator"
 plugin_major = 1
 plugin_minor = 0
-plugin_revision = 2
-plugin_build = 100010
-plugin_version = plugin_major .. '.' .. plugin_minor .. '.' .. plugin_revision .. '.' .. plugin_build
+plugin_revision = 3
+plugin_build = 100011
+plugin_version = string.format('%s.%s.%s.%s', plugin_major, plugin_minor, plugin_revision, plugin_build)
 plugin_id = 'me.teran.lightroom.deduplicator'
 plugin_home_url = "https://github.com/teran/deduplicator"
 latest_release_json_url = 'https://api.github.com/repos/teran/deduplicator/releases/latest'
